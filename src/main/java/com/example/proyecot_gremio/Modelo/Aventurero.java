a0625f0cef77f6ca1f5f3bb18afb9acd862b1f21
Eduardo Barrera <ed.barrerat@duocuc.cl>
2026-05-05 20:22:44 -0400

    Terminé el modelo de party y aventurero

--- src/main/java/com/example/proyecot_gremio/Modelo/Aventurero.java
+++ src/main/java/com/example/proyecot_gremio/Modelo/Aventurero.java
@@ -1,23 +1,43 @@
 package com.example.proyecot_gremio.Modelo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "aventurtero")
 public class Aventurero {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank (message = "El nombre es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Builder.Default
+    @Min(value = 1, message = "El nivel mínimo es 1")
+    @Max(value = 99, message = "El nivel máximo es 99")
+    @Column(nullable = false)
+    private Integer nivel = 1;
 
 }
